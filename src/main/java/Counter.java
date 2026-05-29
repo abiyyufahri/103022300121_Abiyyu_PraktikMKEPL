84dc4ae0cf1a2a18f2f8e42895eff5802d5b4e86
Muhammad Abiyyu Al-Ghifari <49580372+abiyyufahri@users.noreply.github.com>
2026-05-29 08:07:38 +0700

    Refactor Counter class formatting and structure

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -6,20 +6,16 @@ public class Counter {
 	
 	public Counter() {
 		this.reset();
-	}
-		
+	}	
 	public void reset() {
 		count = 0;
 	}
-	
 	public void increment() {
 		count++;
 	}
-	
 	public void decrement() {
 		count--;
 	}
-	
 	public int getCount() {
 		return count;
 	}
